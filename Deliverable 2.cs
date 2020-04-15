2f75bed1bc4533dbd9728193e1abadbe8cf5f528
Jesse <jessepifko@gmail.com>
2020-04-14 22:11:39 -0400

    This is deliverable 2

---
+++ Deliverable 2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Del2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your message?");
+            string input = Console.ReadLine();
+            input = input.ToUpper();
+            Console.WriteLine("Converting input to all uppercase...");
+
+            string message = "";
+            int checksum = 0;
+
+            //Console.WriteLine(input.Length);
+
+            for (int i= 0; i < input.Length; i++)
+            {
+            //Console.WriteLine("i = " + i + " value at index i= " + input[i]);
+                char A = input[i];
+
+                int valA = A - 64;
+
+                message = message + valA + "-";
+
+                checksum = checksum + valA;
+            }
+
+
+            Console.WriteLine("Your coded message is " + message);
+           // Console.WriteLine(checksum);
+
+            Console.WriteLine("Message checksum is " + checksum);
+
+
+        }
+    }
+}
